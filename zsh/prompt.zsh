f6b2b1f3e590373d1805cf1a2faca9907b70fa0b
Hoang Nguyen <htnguyen@axon.com>
2021-05-29 12:06:14 +0700

    Cleanup

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -66,7 +66,18 @@ battery_status() {
   fi
 }
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+last_exit_code() {
+  local LAST_EXIT_CODE=$?
+  if [[ $LAST_EXIT_CODE -ne 0 ]]; then
+    local EXIT_CODE_PROMPT=''
+    EXIT_CODE_PROMPT+="%{$fg[red]%}(%{$reset_color%}"
+    EXIT_CODE_PROMPT+="%{$fg_bold[red]%}$LAST_EXIT_CODE%{$reset_color%}"
+    EXIT_CODE_PROMPT+="%{$fg[red]%})%{$reset_color%}"
+    echo "$EXIT_CODE_PROMPT "
+  fi
+}
+
+export PROMPT=$'\n$(last_exit_code)$(battery_status)in $(directory_name) $(git_dirty)$(need_push) \n›'
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
